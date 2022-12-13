5780dfdfb4859c2881a059fabe935c310e71f35e
SINDNURE <119412287+SINDNURE@users.noreply.github.com>
2022-12-13 10:13:08 +0300

    hilmi sevban erin
    @themanoftalent

--- printYourName.c
+++ printYourName.c
@@ -1,1 +1,19 @@
 // your name surname and age from user
+#include <stdio.h>
+#include <stdlib.h>
+ 
+ 
+int main() {
+	
+	char name[20];
+	int age;
+	
+	
+	printf("Hello, please enter your name and surname");
+	scanf("%s",name);
+	
+	printf("please enter your age");
+	scanf("%d",&age);
+	
+	return 0;
+}
